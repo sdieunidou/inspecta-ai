33a8cd9966bcc7f54683e8f7f265f01fd4755318
seb <s.dieunidou@gmail.com>
2025-11-24 12:34:30 +0100

    add arguments + tests data

--- src/Console/Command/AnalyzeCommand.php
+++ src/Console/Command/AnalyzeCommand.php
@@ -6,12 +6,21 @@ namespace InspectaAi\Console\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'analyze', description: 'Run configurable AI analysis on a target file')]
 class AnalyzeCommand extends Command
 {
+    protected function configure(): void
+    {
+        $this->addArgument('file', InputArgument::REQUIRED, 'The file to analyze');
+        $this->addArgument('prompt', InputArgument::REQUIRED, 'The prompt to use');
+        $this->addArgument('provider', InputArgument::REQUIRED, 'The provider to use (ex: Ollama, OpenAI)');
+        $this->addArgument('model', InputArgument::REQUIRED, 'The model to use (ex: llama3.2, gpt-5.1)');
+    }
+    
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Analyzing...');
